58e478942d3e50d91aac0932a0cc78b5b8aeeee1
vgoret <91573122+victorgrt@users.noreply.github.com>
2024-01-28 18:14:25 +0100

    Add files via upload

--- script_class.sh
+++ script_class.sh
@@ -1,5 +1,50 @@
 #!/bin/bash
 
+# Fonction pour afficher une barre de chargement avec informations
+function afficher_barre_chargement_informations() {
+    local texte="$1"
+    local pourcentage="$2"
+    local largeur=30
+    local delay=0.005
+
+    # Calculer le nombre de caractères à afficher pour le pourcentage
+    local chars_pourcentage=$((($pourcentage * $largeur) / 100))
+
+    echo -n -e "$texte ["
+
+    local i
+    for i in $(seq 1 $chars_pourcentage); do
+        echo -n "#"
+        sleep $delay
+    done
+
+    # Afficher les espaces vides
+    for i in $(seq 1 $((largeur - chars_pourcentage))); do
+        echo -n " "
+    done
+
+    echo -e -n "] $pourcentage%"
+}
+
+# Fonction pour afficher une étape du script avec couleur et délai
+function afficher_etape_script() {
+    local texte="$1"
+    local couleur="$2"
+    local delay=0.3
+
+    echo -e -n "$couleur$texte$RESET"
+    sleep $delay
+}
+
+# Définir les couleurs de texte
+RESET="\\033[0m"
+BOLD="\\033[1m"
+GREEN="\\033[1;32m"
+BLUE="\\033[1;34m"
+YELLOW="\\033[1;33m"
+MAGENTA="\\033[1;35m"
+RED="\\033[1;31m"
+
 # Vérifier si le nombre d'arguments est correct
 if [ $# -lt 1 ]; then
     echo "Usage: $0 <nom_du_projet> [DELETE]"
@@ -17,10 +62,21 @@ if [ "$2" == "DELETE" ]; then
     exit 0
 fi
 
+# Afficher la barre de chargement pour l'ensemble du script
+echo -e "$BOLD\nScript en cours d'exécution...$RESET"
+pourcentage=0
+
 # Créer le fichier Makefile
+afficher_etape_script "Création du fichier Makefile" "$BLUE"
+sleep 0.5
 echo -e "NAME = $projet\n\nCXX = c++\nCXXFLAGS = -Wall -Wextra -Werror -std=c++98\n\nSRCS = main.cpp $projet.cpp\nOBJS = \$(SRCS:.cpp=.o)\n\nall: \$(NAME)\n\n\$(NAME): \$(OBJS)\n\t\$(CXX) \$(CXXFLAGS) -o \$@ \$^\n\n%.o: %.cpp\n\t\$(CXX) \$(CXXFLAGS) -c \$< -o \$@\n\nclean:\n\trm -f \$(OBJS)\n\nfclean: clean\n\trm -f \$(NAME)\n\nre: fclean all\n\n.PHONY: all clean fclean re" > Makefile
+pourcentage=25
+afficher_barre_chargement_informations " Progression globale du script" $pourcentage
+echo -e " - $GREEN Fichier Makefile créé avec succès$RESET"
 
 # Créer le fichier main.cpp
+afficher_etape_script "Création du fichier main.cpp" "$YELLOW"
+sleep 0.5
 echo -e "#include \"$projet.hpp\"\n\n \
 int main()\
 \n{\n\
@@ -28,16 +84,25 @@ int main()\
 \t$projet your$projet = my$projet;\n\
 \treturn 0;\n}" \
 > main.cpp
-
+pourcentage=50
+afficher_barre_chargement_informations " Progression globale du script" $pourcentage
+echo -e " - $GREEN Fichier main.cpp créé avec succès$RESET"
 # Créer le fichier .cpp
+afficher_etape_script "Création du fichier $projet.cpp" "$MAGENTA"
+sleep 0.5
 echo -e "#include \"$projet.hpp\"\n\n\
-$projet::$projet()\n{\n\tstd::cout << GREEN << \"Constructeur par défault called.\" << RESET << std::endl;\n}\n\n\
+$projet::$projet()\n{\n\tstd::cout << GREEN << \"Constructeur par défaut called.\" << RESET << std::endl;\n}\n\n\
 $projet::$projet(const $projet& copie)\n{\n\t(void) copie;\n\tstd::cout << BLUE << \"Constructeur par copie called.\" << RESET << std::endl;\n}\n\n\
 $projet::~$projet()\n{\n\tstd::cout << RED << \"Destructeur called.\" << RESET << std::endl;\n}\n\n\
 $projet& $projet::operator=(const $projet& copie)\n{\n\t(void) copie;\n\tstd::cout << YELLOW << \"Operateur d\'affectation called.\" << RESET << std::endl;\treturn *this;\n}\n" \
 > $projet.cpp
+pourcentage=75
+afficher_barre_chargement_informations " Progression globale du script" $pourcentage
+echo -e " - $GREEN Fichier $projet.cpp créé avec succès$RESET"
 
 # Créer le fichier .hpp
+afficher_etape_script "Création du fichier $projet.hpp" "$RED"
+sleep 0.5
 echo -e "#ifndef ${projet^^}_HPP\n#define ${projet^^}_HPP\n\n\
 #include <iostream>\n\n\
 #define RESET \"\\033[0m\"\n\
@@ -57,7 +122,27 @@ class $projet\n\
 \t$projet& operator=(const $projet& copie);\n\n\
 };\n\n\
 #endif" > $projet.hpp
-
+pourcentage=100
+afficher_barre_chargement_informations " Progression globale du script" $pourcentage
+echo -e " - $GREEN Fichier $projet.hpp créé avec succès$RESET"
 # Afficher un message de confirmation
-echo "Fichiers générés avec succès pour le projet $projet."
+longueur_nom=${#projet}
 
+
+# Longueur totale de la ligne (40 caractères)
+longueur_totale=33
+
+# Calculer l'espace à ajouter après le nom de la classe
+espace_apres_nom=$((longueur_totale - longueur_nom - 2))
+
+# Utiliser printf pour assurer un alignement correct
+echo -e "$GREEN╔═════════════════════════════════════╗\n\
+║                                     ║\n\
+║            Fichiers Créés           ║\n\
+║                                     ║\n\
+║ $BLUE Makefile$RESET$GREEN                           ║\n\
+║ $YELLOW main.cpp$RESET$GREEN                           ║\n\
+║  $MAGENTA$(printf "%-${longueur_nom}s" "$projet.cpp")$(printf "%-${espace_apres_nom}s" " ")$RESET$GREEN║\n\
+║  $RED$(printf "%-${longueur_nom}s" "$projet.hpp")$(printf "%-${espace_apres_nom}s" " ")$RESET$GREEN║\n\
+║                                     ║\n\
+╚═════════════════════════════════════╝$RESET"
